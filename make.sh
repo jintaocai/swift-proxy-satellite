8fe6567f6132968635f791db5ccfc294c75ff664
Johnny Wang <jwang@swiftstack.com>
2018-05-17 12:40:51 -0700

    add more info for make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,10 @@
+# if swiftstack swift node /opt/ss/bin
+# install required binaries
+pip install -r requirements.txt
 # python setup.py install
 python ./setup.py sdist
-pip install ./dist/proxy_satellite-0.0.1.tar.gz --upgrade
-# pip install ./dist/proxy_satellite-0.0.1.tar.gz
+# pip install ./dist/proxy_satellite-0.0.1.tar.gz --upgrade
+pip install ./dist/proxy_satellite-0.0.1.tar.gz
 swift-init proxy-server restart
+# systemctl restart ssnoded.service
 tail -f /var/log/swift/proxy_access.log
